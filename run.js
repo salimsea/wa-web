ecd1cf28172d57dc6c2cb0ee53f5ac8aaad77fcd
Salim Segaf <49223890+salimsea@users.noreply.github.com>
2020-05-15 07:19:06 +0700

    update request

--- run.js
+++ run.js
@@ -1,13 +1,27 @@
 const { Client } = require('whatsapp-web.js');
+var request = require('request');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
     console.log('QR RECEIVED', qr);
+	
+	var options = {
+	  'method': 'POST',
+	  'url': 'https://sealikes.com/wapi',
+	  formData: {
+		'qrcode': qr
+	  }
+	};
+	request(options, function (error, response) { 
+	  if (error) throw new Error(error);
+	  console.log(response.body);
+	});
+
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
+
 });
 
 client.on('message', msg => {
